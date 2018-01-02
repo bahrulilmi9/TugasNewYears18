4cd24236ba6d8d3384b867765b56d88cf84221a7
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-01-02 14:41:43 +0800

    Mengambil Nilai Max Tiap Kriteria

--- TugasYears.cpp
+++ TugasYears.cpp
@@ -24,4 +24,11 @@ int main(){
 	cout.setf(ios::fixed);
 	cout.setf(ios::showpoint);
 	cout.precision(4);
+	
+	mc[0]=max(a1c[0],max(a2c[0],a3c[0]));
+	mc[1]=max(a1c[1],max(a2c[1],a3c[1]));
+	mc[2]=max(a1c[2],max(a2c[2],a3c[2]));
+	mc[3]=max(a1c[3],max(a2c[3],a3c[3]));
+	mc[4]=max(a1c[4],max(a2c[4],a3c[4]));
+
 }
