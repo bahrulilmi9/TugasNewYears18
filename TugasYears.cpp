9b3876cb0346a244b96973a66f329e7259324aa9
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-01-02 14:39:17 +0800

    Membuat Deklarasi Variabel

--- TugasYears.cpp
+++ TugasYears.cpp
@@ -3,5 +3,21 @@
 using namespace std;
 
 int main(){
+	// DEKLARASI VARIABEL
 	
+	double a1c[5]={4,4,5,3,3};
+	double a2c[5]={3,3,4,2,3};
+	double a3c[5]={5,4,2,2,2};
+	
+	double Ra1c[5];
+	double Ra2c[5];
+	double Ra3c[5];
+	
+	double RWa1c[5];
+	double RWa2c[5];
+	double RWa3c[5];
+		
+	double w[5]={5,3,4,4,2};
+	double mc[5];
+	double ha1,ha2,ha3;
 }
