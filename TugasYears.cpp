2261a89d8c514deffde8f56a72c58c3abf61528a
bahrulilmi9 <32448422+bahrulilmi9@users.noreply.github.com>
2018-01-02 14:52:43 +0800

    Penjumlahan Hasil dan Menampilkan Nilai

--- TugasYears.cpp
+++ TugasYears.cpp
@@ -117,4 +117,33 @@ int main(){
 	RWa3c[3] = Ra3c[3]*w[3];
 	RWa3c[4] = Ra3c[4]*w[4];
 
+	// PENJUMLAHAN HASIL R * W
+	
+	ha1=RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4];
+	ha2=RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4];
+	ha3=RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4];
+		
+	// MENAMPILKAN NILAI
+	cout << "MATRIKS X :"<<endl;
+	cout << a1c[0] <<" | "<<a1c[1]<<" | "<<a1c[2]<<" | "<<a1c[3]<<" | "<<a1c[4]<<endl;
+	cout << a2c[0] <<" | "<<a2c[1]<<" | "<<a2c[2]<<" | "<<a2c[3]<<" | "<<a2c[4]<<endl;
+	cout << a3c[0] <<" | "<<a3c[1]<<" | "<<a3c[2]<<" | "<<a3c[3]<<" | "<<a3c[4]<<endl;
+	cout << endl;
+	cout << "MATRIKS R :"<<endl;
+	cout << Ra1c[0] <<" | "<<Ra1c[1]<<" | "<<Ra1c[2]<<" | "<<Ra1c[3]<<" | "<<Ra1c[4]<<endl;
+	cout << Ra2c[0] <<" | "<<Ra2c[1]<<" | "<<Ra2c[2]<<" | "<<Ra2c[3]<<" | "<<Ra2c[4]<<endl;
+	cout << Ra3c[0] <<" | "<<Ra3c[1]<<" | "<<Ra3c[2]<<" | "<<Ra3c[3]<<" | "<<Ra3c[4]<<endl;
+	cout << endl;
+	cout << "BOBOT W :"<<endl;
+	cout << w[0] <<" | "<<w[1]<<" | "<<w[2]<<" | "<<w[3]<<" | "<<w[4]<<endl;
+	cout << endl;
+	cout << "MATRIKS R * W :"<<endl;
+	cout << RWa1c[0] <<" | "<<RWa1c[1]<<" | "<<RWa1c[2]<<" | "<<RWa1c[3]<<" | "<<RWa1c[4]<<endl;
+	cout << RWa2c[0] <<" | "<<RWa2c[1]<<" | "<<RWa2c[2]<<" | "<<RWa2c[3]<<" | "<<RWa2c[4]<<endl;
+	cout << RWa3c[0] <<" | "<<RWa3c[1]<<" | "<<RWa3c[2]<<" | "<<RWa3c[3]<<" | "<<RWa3c[4]<<endl;
+	cout << endl;
+	cout << "HASIL AKHIR  :"<<endl;
+	cout << "Nilai A1 : "<<ha1<<endl;
+	cout << "Nilai A2 : "<<ha2<<endl;
+	cout << "Nilai A3 : "<<ha3<<endl;
 }
